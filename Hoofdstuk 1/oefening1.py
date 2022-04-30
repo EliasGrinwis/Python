fcc98585e44b9800dabc5fbe931df6ba301a3ca1
EliasGrinwis <r0892566@student.thomasmore.be>
2022-04-30 21:22:17 +0200

    Updated

--- Hoofdstuk 1/oefening1.py
+++ Hoofdstuk 1/oefening1.py
@@ -1,1 +1,10 @@
-print("hello")
+surName = str(input("surname: "))
+firstName = str(input("first name: "))
+street = str(input("street: "))
+number = int(input("number: "))
+zipCode = int(input("zip code: "))
+city = str(input("city: "))
+
+print(firstName, surName)
+print(street, number)
+print(zipCode, city)
